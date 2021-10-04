af8fcca4c48fd3475356f20ed5d04d5ba3734911
bneeruga <bneeruga@buffalo.edu>
2021-10-04 13:50:43 -0400

    accelometer activity commit 1

--- app/src/main/java/com/bneeruga/testbeta/MainActivity.java
+++ app/src/main/java/com/bneeruga/testbeta/MainActivity.java
@@ -2,13 +2,27 @@ package com.bneeruga.testbeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button accButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        accButton = findViewById(R.id.btn1);
+        accButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Accelo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
